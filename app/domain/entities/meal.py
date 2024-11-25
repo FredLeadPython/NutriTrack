ff777581e596e2b54e32014a702cbe7a7d42389d
Fred <frederic.juppont@skillcorner.com>
2024-11-25 17:41:27 +0100

    create entities from Response schema

--- app/domain/entities/meal.py
+++ app/domain/entities/meal.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from uuid import UUID, uuid4
+
+from schemas.meal_schemas import MealResponse
 
 
 class Meal:
@@ -9,7 +10,14 @@ class Meal:
         dietary_preferences: Optional[str],
         allergens: Optional[str],
     ):
-        self.id: UUID = uuid4()
         self.type = type
         self.dietary_preferences = dietary_preferences
         self.allergens = allergens
+
+    @classmethod
+    def from_meal_result(meal_result: MealResponse):
+        return Meal(
+            type=meal_result.type,
+            dietary_preferences=meal_result.dietary_preferences,
+            allergens=meal_result.allergens,
+        )
--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -1,10 +1,16 @@
 from typing import Optional
-from uuid import UUID, uuid4
+
+from schemas.user_schemas import UserResult
 
 
 class User:
     def __init__(self, username: str, email: str, age: Optional[int] = None):
-        self.id: UUID = uuid4()
         self.username = username
         self.email = email
         self.age = age
+
+    @classmethod
+    def from_user_result(user_result: UserResult):
+        return User(
+            username=user_result.username, email=user_result.email, age=user_result.age
+        )
